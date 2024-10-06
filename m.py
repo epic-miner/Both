7b70bc21217a389fb0b19a65e1f02c3b689e1227
epic miner <155073729+epic-miner@users.noreply.github.com>
2024-10-06 07:47:37 +0100

    Update m.py

--- m.py
+++ m.py
@@ -8,10 +8,10 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot token here
-bot = telebot.TeleBot('6306399777:AAGO0tUcRAL_jbApX45y8VwBqCCQ6gXa5uw')
+bot = telebot.TeleBot('6577947263:AAFCSaO58IjUSTr_r9AkSPhwaoYJBmhLC78')
 
 # Admin user IDs
-admin_id = ["1436266377"]
+admin_id = ["6365909887"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
